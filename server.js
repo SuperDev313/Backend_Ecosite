4aefe44beac7e4c8db2ba898a7c9eb04fc245a93
superDev313 <pablo313boss@gmail.com>
2023-10-16 20:17:11 -0600

    cors error fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+app.use(cors());
 // Init Middleware
 app.use(express.json());
 
